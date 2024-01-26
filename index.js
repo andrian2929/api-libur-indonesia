2b833862fa173e4eda90c936c9e3d13bfd75c2c2
Andrian Putra Ramadan <ramadanandrian89@gmail.com>
2024-01-27 01:24:03 +0700

    Update User-Agent in fetchPublicHolidays function

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ const fetchPublicHolidays = async (year) => {
     url: HOLIDAYS_URL,
     headers: {
       'User-Agent':
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
+        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0',
     },
   };
 
